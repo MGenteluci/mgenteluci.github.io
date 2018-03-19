e52a05840c7db00e807afc2089c68f6d2b0bce23
mgenteluci <mgenteluci@lumis.com.br>
2018-03-19 12:24:30 -0300

    Mudança de background e adição de ícone de email

--- js/particles.js
+++ js/particles.js
@@ -1,33 +1,33 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 6, density: { enable: true, value_area: 800 } },
-    color: { value: "#1b1e34" },
+    number: { value: 60, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
     shape: {
       type: "polygon",
-      stroke: { width: 0, color: "#000" },
-      polygon: { nb_sides: 6 },
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
       image: { src: "img/github.svg", width: 100, height: 100 }
     },
     opacity: {
-      value: 0.3,
-      random: true,
+      value: 0.5,
+      random: false,
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
     },
     size: {
-      value: 160,
-      random: false,
-      anim: { enable: true, speed: 10, size_min: 40, sync: false }
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
     },
     line_linked: {
-      enable: false,
-      distance: 200,
+      enable: true,
+      distance: 150,
       color: "#ffffff",
-      opacity: 1,
-      width: 2
+      opacity: 0.4,
+      width: 1
     },
     move: {
       enable: true,
-      speed: 8,
+      speed: 6,
       direction: "none",
       random: false,
       straight: false,
@@ -37,16 +37,16 @@ particlesJS("particles-js", {
     }
   },
   interactivity: {
-    detect_on: "canvas",
+    detect_on: "window",
     events: {
-      onhover: { enable: false, mode: "grab" },
+      onhover: { enable: true, mode: "grab" },
       onclick: { enable: false, mode: "push" },
       resize: true
     },
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
+      repulse: { distance: 60, duration: 0.4 },
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 }
     }
