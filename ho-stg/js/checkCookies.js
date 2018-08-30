ed9319407e846f371d271e38768d0dfc3ba697a7
Matheus Genteluci <mgenteluci97@gmail.com>
2018-08-30 14:46:45 -0300

    checkCookies mais preciso

--- ho-stg/js/checkCookies.js
+++ ho-stg/js/checkCookies.js
@@ -4,7 +4,13 @@
         return cookie.split('=');
     });
 
-    if(cookies.length < 6)
+	let count = 0;
+	cookies.forEach(c => {
+        if(c[0] === 'token' || c[0] === 'name' || c[0] === 'userId' || c[0] === 'teamId' || c[0] === 'teamChatUrl' || c[0] === 'userRole')
+			count++;
+    });
+	
+    if(count != 6)
         window.location.href = 'index.html';
 
 })();
